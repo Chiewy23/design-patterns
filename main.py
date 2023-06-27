06483c20b7c25ddf117e4eab1fd8b37f24cf047e
Christopher Hiew <christopherhiew@protonmail.com>
2023-06-27 17:09:08 +0100

    .gitignore updated.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from singleton.singleton import Singleton
 
 
 # ----- SINGLETON PATTERN -----
-singleton1 = Singleton()
-singleton2 = Singleton()
+singleton1 = Singleton("Bob")
+singleton2 = Singleton("Charlie")
 
 print(singleton1 is singleton2)
 

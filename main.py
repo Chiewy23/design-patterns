41b945cff56c114802a171979a8d97a4f8efc311
Christopher Hiew <christopherhiew@protonmail.com>
2023-06-28 16:52:09 +0100

    Demo'ing field and instance variables.

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from singleton.singleton import Singleton
 
 # ----- SINGLETON PATTERN -----
 singleton1 = Singleton(name="Bob")
-singleton2 = Singleton()
+print(singleton1.name)
+print(Singleton.name)
+
+singleton2 = Singleton(name="Trevor")
+print(singleton1.name)
+print(Singleton.name)
 
 print(singleton1 is singleton2)
-print(singleton1.name)
 
 
 if __name__ == '__main__':

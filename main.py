4fb6a34d9c0c0d1ccb6c68967a17a018a053fea3
Christopher Hiew <christopherhiew@protonmail.com>
2023-06-28 16:40:45 +0100

    Latest version of Singleton.

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from singleton.singleton import Singleton
 
 
 # ----- SINGLETON PATTERN -----
-singleton1 = Singleton("Bob")
-singleton2 = Singleton("Charlie")
+singleton1 = Singleton(name="Bob")
+singleton2 = Singleton()
 
 print(singleton1 is singleton2)
+print(singleton1.name)
+
 
 if __name__ == '__main__':
     pass
--- singleton/singleton.py
+++ singleton/singleton.py
@@ -1,11 +1,10 @@
 class Singleton:
-    instance = None
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "instance"):
             cls.instance = super(Singleton, cls).__new__(cls)
+            cls.name = kwargs["name"]
         return cls.instance
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         pass
-

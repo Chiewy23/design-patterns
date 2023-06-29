c06bd2f15d25e4d95165c910700a75603bc42b30
Christopher Hiew <christopherhiew@protonmail.com>
2023-06-29 21:38:42 +0100

    Grouping examples in functions

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from singleton.singleton import Singleton
 
 
 # ----- SINGLETON PATTERN -----
-singleton1 = Singleton()
-singleton2 = Singleton()
+def singleton_example():
+    singleton1 = Singleton()
+    singleton2 = Singleton()
 
-print(singleton1 is singleton2)
+    print(singleton1 is singleton2)
+
+
+def factory_example():
+    pass
 
 
 if __name__ == '__main__':
-    pass
+    singleton_example()
 

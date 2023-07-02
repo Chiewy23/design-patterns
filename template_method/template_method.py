6b56f502841c84e1f00f9da9ab86bf2dfc4d2ed4
Christopher Hiew <christopherhiew@protonmail.com>
2023-07-02 17:23:29 +0100

    Classes created.

--- template_method/template_method.py
+++ template_method/template_method.py
@@ -1,3 +1,23 @@
+class ExcelFile:
+    def read_data(self):
+        print(f"Reads data from excel file - {self}")
+
+    def process_data(self):
+        print(f"Processes data from excel file - {self}")
+
+    def save_data(self):
+        print(f"Saves data to Db - {self}")
+
+
+class TextFile:
+    def read_data(self):
+        print(f"Reads data from text file - {self}")
+
+    def process_data(self):
+        print(f"Processes data from text file - {self}")
+
+    def save_data(self):
+        print(f"Saves data to Db - {self}")
 
 
 if __name__ == "__main__":

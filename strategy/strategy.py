fff1948d3d9a62c4d2443b42ed64e98f17454c6e
Christopher Hiew <christopherhiew@protonmail.com>
2023-07-06 19:56:16 +0100

    Context method created.

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -22,5 +22,13 @@ class Subtract(StrategyBase):
         return int1 - int2
 
 
+class Context:
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+    def execute_strategy(self, int1, int2):
+        return self.strategy.execute(int1, int2)
+
+
 if __name__ == "__main__":
     pass

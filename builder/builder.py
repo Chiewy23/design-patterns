724e0a7459f44841dd76c863333abd9ba778c8f3
Christopher Hiew <christopherhiew@protonmail.com>
2023-07-15 16:12:33 -0700

    Default params added.

--- builder/builder.py
+++ builder/builder.py
@@ -19,8 +19,34 @@ class InsuranceBuilderBase:
         pass
 
 
+class Driver:
+    def __init__(self, name="No Name", age=99):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} ({self.age})"
+
+
+class Location:
+    def __init__(self, zip_code="No Zipcode"):
+        self.zip_code = zip_code
+
+    def __str__(self):
+        return f"{self.zip_code}"
+
+
+class Vehicle:
+    def __init__(self, model="No model", year=9999):
+        self.model = model
+        self.year = year
+
+    def __str__(self):
+        return f"{self.model} ({self.year})"
+
+
 class Insurance:
-    def __init__(self, driver, location, vehicle, price):
+    def __init__(self, driver=Driver(), location=Location(), vehicle=Vehicle(), price=0.00):
         self.driver = driver
         self.location = location
         self.vehicle = vehicle
@@ -30,33 +56,10 @@ class Insurance:
         return f"Insurance has these values: \n {self.driver} \n {self.location} \n {self.vehicle} \n {self.price}"
 
 
-class Driver:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __str__(self):
-        return f"{self.name} ({self.age})"
-
-
-class Location:
-    def __init__(self, zip_code):
-        self.zip_code = zip_code
-
-    def __str__(self):
-        return f"{self.zip_code}"
-
-
-class Vehicle:
-    def __init__(self, model, year):
-        self.model = model
-        self.year = year
-
-    def __str__(self):
-        return f"{self.model} ({self.year})"
-
-
 class AutoBuilder(InsuranceBuilderBase):
+    def __init__(self):
+        self.insurance = Insurance()
+
     def build_driver(self):
         pass
 

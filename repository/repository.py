e3053f95876b6e7e2877806e1995a05e3d2e746d
Christopher Hiew <christopherhiew@protonmail.com>
2023-07-27 15:52:08 +0100

    Repository classes complete.

--- repository/repository.py
+++ repository/repository.py
@@ -1,3 +1,6 @@
+from abc import abstractmethod
+
+
 class Contact:
     def __init__(self, name):
         self.name = name
@@ -22,3 +25,66 @@ class Work(Contact):
     def __init__(self, email, name):
         super().__init__(name)
         self.email = email
+
+
+class RepositoryBase:
+    @abstractmethod
+    def append(self, item):
+        pass
+
+    @abstractmethod
+    def remove(self, item):
+        pass
+
+    @abstractmethod
+    def get_all(self):
+        pass
+
+
+class InMemoryList(RepositoryBase):
+    def __init__(self):
+        self.list = []
+
+    def append(self, item):
+        self.list.append(item)
+
+    def remove(self, item):
+        self.list.remove(item)
+
+    def get_all(self):
+        result = ""
+
+        for item in self.list:
+            result += f"{item}\n"
+
+        return result
+
+
+class TextFileRepository(RepositoryBase):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def append(self, item):
+        print("Appending to text file.")
+
+    def remove(self, item):
+        print("Removing from text file.")
+
+    def get_all(self):
+        print("Printing all items in text file.")
+
+
+class ContactUI:
+    def __init__(self, repository):
+        if repository is not RepositoryBase:
+            raise Exception("Repository must be of type RepositoryBase")
+        self.repository = repository
+
+    def append(self, contact):
+        self.repository.add(contact)
+
+    def remove(self, contact):
+        self.repository.remove(contact)
+
+    def get_all_contacts(self):
+        print(self.repository.get_all())
